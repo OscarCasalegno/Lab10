7a4dea8d4df7ca3cbe009581e8a083b4b4a82e96
OscarAlberto <s245739@studenti.polito.it>
2020-05-26 17:21:36 +0200

    soluzione

--- src/main/java/it/polito/tdp/bar/model/Event.java
+++ src/main/java/it/polito/tdp/bar/model/Event.java
@@ -1,5 +1,31 @@
 package it.polito.tdp.bar.model;
 
-public class Event {
+import java.time.LocalTime;
+
+public class Event implements Comparable<Event> {
+
+	private LocalTime time;
+
+	/**
+	 * @param time
+	 * @param type
+	 */
+	public Event(LocalTime time) {
+		super();
+		this.time = time;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	@Override
+	public int compareTo(Event o) {
+		return this.time.compareTo(o.getTime());
+	}
 
 }
